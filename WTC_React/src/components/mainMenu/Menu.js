66d9096523016f31562f9c13086d3e7e5e6a9036
Martin Burza <martinburza@outlook.cz>
2018-10-06 12:41:29 +0200

    Create ModuleRenderer in services folder

--- WTC_React/src/components/mainMenu/Menu.js
+++ WTC_React/src/components/mainMenu/Menu.js
@@ -1,33 +1,8 @@
 import React, {Component} from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
-import modulConfig from '../../modulConfig.json';
+
+import ModuleRenderer from '../../services/ModuleRenderer';
 import AppSettings from './AppSettings';
-
-class ModuleRenderer extends Component {
-  state = {
-    modules: []
-  };
-
-  componentDidMount () {
-    // load the module from JSON config
-    modulConfig.map (item => {
-      if (this.props.position === item.menuItemPosition) {
-        import (`../../${item.menuItemPath}`).then (module => {
-          this.state.modules.push (module.default);
-          this.setState ({modules: this.state.modules});
-        });
-      }
-    });
-  }
-
-  render () {
-    return (
-      <React.Fragment>
-        {this.state.modules.map ((ModuleItem, index) => <ModuleItem key={index}/>)}
-      </React.Fragment>
-    );
-  }
-}
 
 export default class Menu extends Component {
   state = {
---
+++ WTC_React/src/services/ModuleRenderer.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import modulConfig from '../modulConfig.json';
+
+export default class ModuleRenderer extends Component {
+    state = {
+        modules: []
+    };
+
+    componentDidMount () {
+        // load the module from JSON config
+        modulConfig.map (item => {
+        if (this.props.position === item.menuItemPosition) {
+            import (`../${item.menuItemPath}`).then (module => {
+                this.state.modules.push (module.default);
+                this.setState ({modules: this.state.modules});
+            });
+        }
+        });
+    }
+
+    render () {
+        return (
+        <React.Fragment>
+            {this.state.modules.map ((ModuleItem, index) => <ModuleItem key={index}/>)}
+        </React.Fragment>
+        );
+    }
+}
